fcc07736af5d8585d14bbeeef8fe7d91d242f95d
VladUkhatyuk <vladukhatyuk@gmail.com>
2018-10-04 13:34:42 +0100

    Changed loading animation Added intent to bring to log in activity

--- app/src/main/java/com/halaltokens/halaltokens/SplashScreen.java
+++ app/src/main/java/com/halaltokens/halaltokens/SplashScreen.java
@@ -1,13 +1,32 @@
 package com.halaltokens.halaltokens;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int WELCOME_TIMEOUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, LoginScreen.class);
+                startActivity(i);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, WELCOME_TIMEOUT);
     }
 }
