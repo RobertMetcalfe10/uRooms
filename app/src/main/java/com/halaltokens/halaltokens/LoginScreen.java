e233dac6e21b8734410534adfb190f18e049e14e
VladUkhatyuk <vladukhatyuk@gmail.com>
2018-10-10 14:32:11 +0100

    Started work on user login

--- app/src/main/java/com/halaltokens/halaltokens/LoginScreen.java
+++ app/src/main/java/com/halaltokens/halaltokens/LoginScreen.java
@@ -1,11 +1,13 @@
 package com.halaltokens.halaltokens;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -13,6 +15,20 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Button sign_up_button = findViewById(R.id.sign_up_button);
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
+
+    }
+
+    public void openSignUpActivity() {
+        Intent intent = new Intent(this, RegistrationScreen.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/halaltokens/halaltokens/RegistrationScreen.java
+++ app/src/main/java/com/halaltokens/halaltokens/RegistrationScreen.java
@@ -1,13 +1,116 @@
 package com.halaltokens.halaltokens;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegistrationScreen extends AppCompatActivity {
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class RegistrationScreen extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
+    private LottieAnimationView signUpProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        signUpProgress = findViewById(R.id.sign_up_progress);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.reg_button).setOnClickListener(this);
+        findViewById(R.id.already_user_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.reg_button:
+                registerUser();
+                break;
+
+            case R.id.already_user_button:
+                openLoginActivity();
+                break;
+        }
+    }
+
+
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginScreen.class);
+        startActivity(intent);
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid UCD email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!email.toLowerCase().contains("ucd.ie") && !email.toLowerCase().contains("ucdconnect.ie")) {
+            editTextEmail.setError("Please enter a valid UCD email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Password should be at least 6 characters long");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        signUpProgress.playAnimation();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                signUpProgress.cancelAnimation();
+                signUpProgress.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User Registration Complete", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "An Error Occurred", Toast.LENGTH_SHORT).show();
+
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "This Email Address is already in Use.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
+
+
