a52f139fb7c65cbacc743fd49a993fd25c4a4000
RobertMetcalfe10 <robert.metcalfe@ucdconnect.ie>
2018-10-14 17:14:36 +0100

    compsci,sci,health,eng

--- app/src/main/java/com/halaltokens/halaltokens/MainActivity.java
+++ app/src/main/java/com/halaltokens/halaltokens/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                             RoomInfo roomInfo = new RoomInfo();
                             roomInfo.setRoomName(doc.title().substring(10));
                             roomInfo.setRoomInfo(doc.getElementById("RB200|0."+i).children());
-                            Log.v("Comp",roomInfo.getOrganization());
+                            Log.v("Comp",roomInfo.toString());
                         } catch (NullPointerException e) {
                             break;
                         }
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
                                 RoomInfo roomInfo = new RoomInfo();
                                 roomInfo.setRoomName(doc.title().substring(10));
                                 roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
-                                Log.v("AG", roomInfo.getOrganization());
+                                Log.v("AG", roomInfo.toString());
                             } catch (NullPointerException e) {
                                 break;
                             }
@@ -132,6 +132,203 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+
+            //science hub
+            threadPoolExecutor.execute(() -> {
+                ArrayList<String> sciHubArraylist = new ArrayList<String>();
+                sciHubArraylist.add(getString(R.string.H0_12));
+                sciHubArraylist.add(getString(R.string.H1_12));
+                sciHubArraylist.add(getString(R.string.H1_13));
+                sciHubArraylist.add(getString(R.string.H1_26));
+                sciHubArraylist.add(getString(R.string.H1_37));
+                sciHubArraylist.add(getString(R.string.H1_49));
+                sciHubArraylist.add(getString(R.string.H1_51));
+                sciHubArraylist.add(getString(R.string.H2_12));
+                sciHubArraylist.add(getString(R.string.H2_18));
+                sciHubArraylist.add(getString(R.string.H2_20));
+                sciHubArraylist.add(getString(R.string.H2_21));
+                sciHubArraylist.add(getString(R.string.H2_22));
+                sciHubArraylist.add(getString(R.string.H2_32));
+                sciHubArraylist.add(getString(R.string.H2_38));
+                sciHubArraylist.add(getString(R.string.H2_40));
+                for (String page : sciHubArraylist) {
+                    try {
+                        Document doc = Jsoup.connect(page).get();
+                        for (int i = 1; i <= 10; i++) {
+                            try {
+                                RoomInfo roomInfo = new RoomInfo();
+                                roomInfo.setRoomName(doc.title().substring(10));
+                                roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
+                                Log.v("SciHub", roomInfo.toString());
+                            } catch (NullPointerException e) {
+                                break;
+                            }
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            //science north
+            threadPoolExecutor.execute(() -> {
+                ArrayList<String> sciNorthArraylist = new ArrayList<String>();
+                sciNorthArraylist.add(getString(R.string.N129));
+                sciNorthArraylist.add(getString(R.string.N231));
+                sciNorthArraylist.add(getString(R.string.N232));
+                for (String page : sciNorthArraylist) {
+                    try {
+                        Document doc = Jsoup.connect(page).get();
+                        for (int i = 1; i <= 10; i++) {
+                            try {
+                                RoomInfo roomInfo = new RoomInfo();
+                                roomInfo.setRoomName(doc.title().substring(10));
+                                roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
+                                Log.v("SciNorth", roomInfo.toString());
+                            } catch (NullPointerException e) {
+                                break;
+                            }
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            //science south
+            threadPoolExecutor.execute(() -> {
+                ArrayList<String> sciSouthArraylist = new ArrayList<String>();
+                sciSouthArraylist.add(getString(R.string.S1_67));
+                sciSouthArraylist.add(getString(R.string.S3_56));
+                for (String page : sciSouthArraylist) {
+                    try {
+                        Document doc = Jsoup.connect(page).get();
+                        for (int i = 1; i <= 10; i++) {
+                            try {
+                                RoomInfo roomInfo = new RoomInfo();
+                                roomInfo.setRoomName(doc.title().substring(10));
+                                roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
+                                Log.v("SciSouth", roomInfo.toString());
+                            } catch (NullPointerException e) {
+                                break;
+                            }
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            //science west
+            threadPoolExecutor.execute(() -> {
+                ArrayList<String> sciWestArraylist = new ArrayList<String>();
+                sciWestArraylist.add(getString(R.string.B101A));
+                sciWestArraylist.add(getString(R.string.B101B));
+                sciWestArraylist.add(getString(R.string.B154A));
+                sciWestArraylist.add(getString(R.string.B154B));
+                for (String page : sciWestArraylist) {
+                    try {
+                        Document doc = Jsoup.connect(page).get();
+                        for (int i = 1; i <= 10; i++) {
+                            try {
+                                RoomInfo roomInfo = new RoomInfo();
+                                roomInfo.setRoomName(doc.title().substring(10));
+                                roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
+                                Log.v("SciWest", roomInfo.toString());
+                            } catch (NullPointerException e) {
+                                break;
+                            }
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            //health science
+            threadPoolExecutor.execute(() -> {
+                ArrayList<String> healthScienceArraylist = new ArrayList<String>();
+                healthScienceArraylist.add(getString(R.string.A003));
+                healthScienceArraylist.add(getString(R.string.A004));
+                healthScienceArraylist.add(getString(R.string.A005));
+                healthScienceArraylist.add(getString(R.string.A006));
+                healthScienceArraylist.add(getString(R.string.A007));
+                healthScienceArraylist.add(getString(R.string.A008));
+                healthScienceArraylist.add(getString(R.string.B004));
+                healthScienceArraylist.add(getString(R.string.B005));
+                healthScienceArraylist.add(getString(R.string.B006));
+                healthScienceArraylist.add(getString(R.string.C004));
+                healthScienceArraylist.add(getString(R.string.C005));
+                healthScienceArraylist.add(getString(R.string.C006));
+                healthScienceArraylist.add(getString(R.string.C007));
+                healthScienceArraylist.add(getString(R.string.C112));
+                healthScienceArraylist.add(getString(R.string.C113));
+                healthScienceArraylist.add(getString(R.string.C114));
+                healthScienceArraylist.add(getString(R.string.C115));
+                healthScienceArraylist.add(getString(R.string.C116));
+                healthScienceArraylist.add(getString(R.string.C117));
+                healthScienceArraylist.add(getString(R.string.C118));
+                healthScienceArraylist.add(getString(R.string.C120));
+                healthScienceArraylist.add(getString(R.string.C122));
+                healthScienceArraylist.add(getString(R.string.C311));
+                healthScienceArraylist.add(getString(R.string.C314));
+                healthScienceArraylist.add(getString(R.string.D114));
+                healthScienceArraylist.add(getString(R.string.D115));
+                healthScienceArraylist.add(getString(R.string.B227));
+                healthScienceArraylist.add(getString(R.string.B237));
+                healthScienceArraylist.add(getString(R.string.B311));
+                healthScienceArraylist.add(getString(R.string.B333));
+                healthScienceArraylist.add(getString(R.string.B341));
+                healthScienceArraylist.add(getString(R.string.B342));
+                healthScienceArraylist.add(getString(R.string.B109));
+                for (String page : healthScienceArraylist) {
+                    try {
+                        Document doc = Jsoup.connect(page).get();
+                        for (int i = 1; i <= 10; i++) {
+                            try {
+                                RoomInfo roomInfo = new RoomInfo();
+                                roomInfo.setRoomName(doc.title().substring(10));
+                                roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
+                                Log.v("HealthScience", roomInfo.toString());
+                            } catch (NullPointerException e) {
+                                break;
+                            }
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            //health science
+            threadPoolExecutor.execute(() -> {
+                ArrayList<String> engArraylist = new ArrayList<String>();
+                engArraylist.add(getString(R.string.E016));
+                engArraylist.add(getString(R.string.E121));
+                engArraylist.add(getString(R.string.E135));
+                engArraylist.add(getString(R.string.E216));
+                engArraylist.add(getString(R.string.E234));
+                engArraylist.add(getString(R.string.E317));
+                engArraylist.add(getString(R.string.E326));
+                for (String page : engArraylist) {
+                    try {
+                        Document doc = Jsoup.connect(page).get();
+                        for (int i = 1; i <= 10; i++) {
+                            try {
+                                RoomInfo roomInfo = new RoomInfo();
+                                roomInfo.setRoomName(doc.title().substring(10));
+                                roomInfo.setRoomInfo(doc.getElementById("RB200|0." + i).children());
+                                Log.v("Eng", roomInfo.toString());
+                            } catch (NullPointerException e) {
+                                break;
+                            }
+                        }
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
 
             return null;
         }
