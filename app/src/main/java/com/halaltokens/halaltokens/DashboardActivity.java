579f5e6b5e305ca2f2c40876d4a661999be7c31d
Abdullah Naveed <muhammad.naveed@ucdconnect.ie>
2018-11-23 13:45:17 +0000

    -images added for bottom nav

--- app/src/main/java/com/halaltokens/halaltokens/DashboardActivity.java
+++ app/src/main/java/com/halaltokens/halaltokens/DashboardActivity.java
@@ -40,10 +40,10 @@ public class DashboardActivity extends AppCompatActivity implements QRFragment.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Home", R.mipmap.ic_launcher);
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Favourites", R.mipmap.ic_launcher);
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("QR", R.mipmap.ic_launcher);
-        AHBottomNavigationItem item4 = new AHBottomNavigationItem("Settings", R.mipmap.ic_launcher);
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Home", R.drawable.ic_home_black_24dp);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Favourites", R.drawable.ic_favorite_black_24dp);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("QR", R.drawable.qrcode);
+        AHBottomNavigationItem item4 = new AHBottomNavigationItem("Settings", R.drawable.ic_settings_black_24dp);
         final AHBottomNavigation ahBottomNavigation = findViewById(R.id.bottom_navigation);
         ahBottomNavigation.addItem(item1);
         ahBottomNavigation.addItem(item2);
