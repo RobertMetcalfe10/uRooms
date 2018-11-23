82ece819abd72eebfbc66d9d1a0bfff31527680a
Erik Burka <eriks.burka@ucdconnect.ie>
2018-11-23 14:39:35 +0000

    -contact us -current user -faq

--- app/src/main/java/com/halaltokens/halaltokens/DashboardActivity.java
+++ app/src/main/java/com/halaltokens/halaltokens/DashboardActivity.java
@@ -158,7 +158,7 @@ public class DashboardActivity extends AppCompatActivity implements PlaceholderF
                 case 2: // Fragment # 0 - This will show FirstFragment
                     return QRFragment.newInstance();
                 case 3: // Fragment # 0 - This will show FirstFragment different title
-                    return PlaceholderFragment.newInstance(position + 3);
+                    return SettingsFragment.newInstance();
                 default:
                     return null;
             }
